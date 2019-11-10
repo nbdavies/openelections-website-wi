c18f961a4d395ca726aaae2617c9f5fe392e885e
Nick Davies <nbdavies@gmail.com>
2019-11-10 16:59:29 -0600

    Load metadata into Vue app

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue!'
-  }
+$(function() {
+  var app = new Vue({
+    el: '#app',
+    data: {
+      metadata: []
+    },
+    created: function () {
+      var metadata_url = 'https://raw.githubusercontent.com/openelections/openelections-data-wi/master/local_data_cache/elections_metadata.json'
+      $.get(metadata_url)
+        .done(function(response){
+          app.metadata = JSON.parse(response).objects
+        })
+    }
+  })
 })
