e595821a1f420c13e64b566a1709460aa0905e75
Nick Davies <nbdavies@gmail.com>
2019-11-11 20:19:00 -0600

    Try to update

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ $(function() {
       var metadata_url = 'https://raw.githubusercontent.com/openelections/openelections-data-wi/master/local_data_cache/elections_metadata.json'
       $.get(metadata_url)
         .done(function(response){
-          var elections = JSON.parse(response).objects.map(function(object){
+          var objects = JSON.parse(response).objects
+          var elections = objects.map(function(object){
             return new Election(object)
           })
           app.elections = elections
