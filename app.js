5e2871671b4d962c53e2d4c3edc3f2ad7704648a
Nick Davies <nbdavies@gmail.com>
2019-11-11 20:15:30 -0600

    Create UI and object class for individual election

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ $(function() {
   var app = new Vue({
     el: '#app',
     data: {
-      metadata: []
+      elections: []
     },
     created: function () {
       var metadata_url = 'https://raw.githubusercontent.com/openelections/openelections-data-wi/master/local_data_cache/elections_metadata.json'
       $.get(metadata_url)
         .done(function(response){
-          app.metadata = JSON.parse(response).objects
+          var elections = JSON.parse(response).objects.map(function(object){
+            return new Election(object)
+          })
+          app.elections = elections
         })
     }
   })
---
+++ election.js
@@ -0,0 +1,24 @@
+class Election {
+  constructor(json) {
+    this.start_date = json.start_date
+    this.portal_link = json.portal_link
+    this.direct_links = json.direct_links
+    this.race_type = json.race_type
+    this.special = json.special
+    this.organizationName = json.organization.name
+    this.id = json.id
+  }
+  year() {
+    return this.start_date.substring(0, 4)
+  }
+  fileName() {
+    var shortDate = this.start_date.replace(/-/g, '')
+    var specialIndicator = (this.special ? 'special__' : '')
+    return shortDate + '__wi__' + specialIndicator + this.race_type + '__ward.csv'
+  }
+  fullPath() {
+    var repoPath = 'https://raw.githubusercontent.com/openelections/openelections-data-wi/master/'
+    var path = repoPath + this.year() + '/'
+    return path + this.fileName()
+  }
+}
